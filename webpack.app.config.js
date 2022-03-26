74a996280f5f48d5e6e85b45889b77b6617cc35e
Sam Ternent <sam.ternent@gmail.com>
2022-03-26 23:55:17 +0000

    postcss and purge

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -50,26 +50,8 @@ module.exports = merge(webpackBase, {
                 plugins: [
                   require("tailwindcss")("./tailwind.config.js"),
                   require("@fullhuman/postcss-purgecss")({
-                    content: ["**/*.html", "**/*.tsx", "**/*.svelte"],
+                    content: ["**/*.html", "**/*.vue"],
                     css: ["**/*.css"],
-                    defaultExtractor: (content) => {
-                      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-                      const broadMatches =
-                        content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-                      const broadMatchesWithoutTrailingSlash = broadMatches.map(
-                        (match) => trimEnd(match, "\\")
-                      );
-
-                      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-                      const innerMatches =
-                        content.match(
-                          /[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g
-                        ) || [];
-
-                      return broadMatches
-                        .concat(broadMatchesWithoutTrailingSlash)
-                        .concat(innerMatches);
-                    },
                   }),
                 ],
               },
